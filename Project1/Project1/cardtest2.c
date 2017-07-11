49b464da448ce5f356ffaccacbe7e0168693892d
MKormann <matt.kormann@gmail.com>
2017-07-11 09:47:58 -0400

    Created test files

---
+++ Project1/Project1/cardtest2.c
@@ -0,0 +1,4 @@
+// Matt Ruglio-Kormann
+// CS362 - Summer 2017
+// Assignment 3 - Unit Tests
+// 07/10/2017
---
+++ Project1/Project1/unittest1.c
@@ -0,0 +1,20 @@
+// Matt Ruglio-Kormann
+// CS362 - Summer 2017
+// Assignment 3 - Unit Tests
+// 07/10/2017
+
+#include 
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+
+testFunction() {
+
+	initializeGame(int numPlayers, int kingdomCards[10], int randomSeed, struct gameState *state)
+
+}
+
+int main() {
+	return 0;
+}
